dfe37c85dae1bdce36482c8f1e9ba6655141219a
inkarnadin <nasyrovvad@gmail.com>
2021-11-30 02:53:08 +0300

    feature: init commit

---
+++ src/main/groovy/ru/alarh/alarhvideomanager/AlarhVideoManagerApplication.groovy
@@ -0,0 +1,13 @@
+package ru.alarh.alarhvideomanager
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AlarhVideoManagerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AlarhVideoManagerApplication, args)
+	}
+
+}
---
+++ src/test/groovy/ru/alarh/alarhvideomanager/AlarhVideoManagerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ru.alarh.alarhvideomanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlarhVideoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
